b38c7812aa549547cd82ccc7f2c7cf885f2ade40
zytronium <dastelljes@outlook.com>
2025-09-05 14:39:42 -0500

    Implemented the social links component

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,13 @@
+export function SocialLinks() {
+  return (
+    <div className="social-links">
+      <a href="https://linkedin.com/in/daniel-stelljes">
+        <img src="/src/assets/linkedin.svg" alt="LinkedIn" />
+      </a>
+      <a href="https://github.com/Zytronium/atlas-react-and-jsx">
+        <img src="/src/assets/github.svg" alt="GitHub" />
+      </a>
+
+    </div>
+  );
+}
