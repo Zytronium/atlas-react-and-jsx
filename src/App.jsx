382e548da3e9b7f5850611caa32ca93be87dda00
zytronium <dastelljes@outlook.com>
2025-09-05 16:20:29 -0500

    Replaced funny placeholders with real information on React

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,30 @@ function App() {
     <div className="app">
       <Header />
       <Section title="What is react?">
-        {/* Todo */}
         React is not a chair. You can&apos;t sit on it, recline on it,
-        or fill empty space in your mildly aesthetically lacking home,
-        and you almost certainly can&apos;t purchase it at any furniture
+        or fill empty space in your mildly aesthetically lacking home with
+        it, and you almost certainly can&apos;t purchase it at any furniture
         or home improvement stores. React doesn&apos;t have any cushioning,
         back support, or physical matter, so if you try to sit on it, you
         will almost certainly fall over and make a fool of yourself in front
         of your very confused family. Even your dog will react in confusion.
+        <br/><br/>
+        Rather than being used for sitting, React is a reactive tool primarily
+        used for front-end web development. It is a library for building reusable
+        UI components. It&apos;s often paired with JSX, a JavaScript syntax
+        extension that makes embedding HTML and React components via JS easier
+        with cleaner and simpler syntax. While React is not considered a
+        framework it is often used in frameworks like Next.js.
       </Section>
 
       <Section title="Benefits of react">
         <ul>
-          {/* Todo */}
-          <li>React is better than a chair</li>
-          <li>React is reactive</li>
-          <li>React is called React (it&apos;s a very attractive name)</li>
-          <li>I like turtles</li>
-          <li>React likes turtles too</li>
+          <li>React is better than a chair (and it&apos;s free!)</li>
+          <li>React is reactive (the DOM reacts to changes instantly in dev environment)</li>
+          <li>Cleaner and easier UI development (with the help of JSX)</li>
+          <li>Makes UI code reusable through components</li>
+          <li>Widely supported</li>
+          <li>Supports a large and active ecosystem of third-party libraries to simplify front-end development</li>
         </ul>
       </Section>
     </div>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,6 +1,7 @@
 export function Section(props) {
   return (
     <div className="section">
+      {/* NOTE: Demo version uses h2 here. Task instructions say h1. */}
       <h1>{props.title}</h1>
       {props.children}
     </div>
